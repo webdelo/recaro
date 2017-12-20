bb8dc7266907e7ec9d6aabf2f95ebe31eba4ac99
benefit50 <d.godiac@webdelo.org>
2017-12-20 14:22:26 +0200

    New section block `Хиты продаж` added

---
+++ js/modalHandler.js
@@ -0,0 +1,87 @@
+$(function(){
+
+    $(document).on('click', '.makeOrderClick', function (){
+        var targetModal = $($(this).data('target'));
+        targetModal.find('h3.form-name').text($(this).data('h3'));
+        targetModal.find('h5').text($(this).data('h5'));
+        targetModal.find('img.main_img').attr("src", $(this).data('image'));
+        targetModal.find('li.first_param').text($(this).data('firstparam'));
+        targetModal.find('li.second_param').text($(this).data('secondparam'));
+        targetModal.find('.price-block').text($(this).data('price'));
+        targetModal.find('.sendRequestButton').data('product', $(this).data('h3'));
+    });
+
+    $(document).on('click', '.sendRequestButton', function (event){
+
+        var form = $('.productRequestForm')[0];
+
+        if(form.checkValidity() === false) {
+            return true;
+        }
+
+        event.preventDefault();
+
+        $.ajax({
+            url: '/mailer.php',
+            type: 'POST',
+            data: {
+                name: $(form).find('.nameField').val(),
+                email: $(form).find('.emailField').val(),
+                phone: $(form).find('.phoneField').val(),
+                address: $(form).find('.addressField').val(),
+                quantity: $(form).find('.quantityField').val(),
+                product: $(form).find('h3.form-name').text(),
+                mailType: 'requestProduct'
+            },
+            dataType: 'json',
+            success: function(response){
+                if(response === 1){
+                    $(form).find('.resultOk').show('fast');
+                } else {
+                    $(form).find('.resultFail').show('fast');
+                }
+            }
+        });
+
+    });
+
+
+    $(document).on('click', '.sendContactButton', function (event){
+
+        var form = $('.contactManagerForm')[0];
+
+        if(form.checkValidity() === false) {
+            return true;
+        }
+
+        event.preventDefault();
+
+        $.ajax({
+            url: '/mailer.php',
+            type: 'POST',
+            data: {
+                name: $(form).find('.contactName').val(),
+                phone: $(form).find('.contactPhone').val(),
+                text: $(form).find('.contactText').val(),
+
+                mailType: 'contactManager'
+            },
+            dataType: 'json',
+            success: function(response){
+                if(response === 1){
+                    $(form).find('.resultOk').show('fast');
+                } else {
+                    $(form).find('.resultFail').show('fast');
+                }
+            }
+        });
+
+    });
+
+    $('.switchProductColor').click(function () {
+        var image = $(this).data('image');
+        var parent = $(this).closest('.white-b-s').find('img.main-share-image');
+        $(parent).attr('src', image);
+    });
+
+});
---
+++ mailer.php
@@ -0,0 +1,60 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+if (isset($_POST['mailType']) && !empty($_POST['mailType'])) {
+
+    $mailType = $_POST['mailType'];
+
+    if ($mailType == 'contactManager') {
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $text = $_POST['text'];
+
+        $emailContent = file_get_contents('./emails/contactManager.htm');
+        $variables = ['name' => $name, 'phone' => $phone, 'text' => $text];
+        $mailSubject = 'Заявка на вопрос менеджеру';
+    } else {
+
+        $quantity = $_POST['quantity'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $product = $_POST['product'];
+
+        $emailContent = file_get_contents('./emails/requestProduct.htm');
+        $variables = [
+            'quantity' => $quantity,
+            'name' => $name,
+            'email' => $email,
+            'phone' => $phone,
+            'address' => $address,
+            'product' => $product,
+        ];
+        $mailSubject = 'Заявка на товар';
+    }
+
+    $host = $_SERVER['HTTP_HOST'];
+    $variables['host'] = $host;
+    foreach ($variables as $key => $val) {
+        $emailContent = str_replace('$' . $key, $val, $emailContent);
+    }
+
+    require_once './vendor/autoload.php';
+
+    $mailSubject .= ' ' . '(' . $host . ')';
+    $recipient = $host == 'recaro-cs.ru' ? 'spb@recaro-cs.ru' : 'moldova@ligood.eu';
+
+    $transport = new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
+    $mailer = new Swift_Mailer($transport);
+
+    $message = (new Swift_Message())
+        ->setSubject($mailSubject)
+        ->setFrom('noreply@'.$host)
+        ->setTo($recipient)
+        ->setPriority(1)
+        ->setBody($emailContent, 'text/html');
+
+    $result = $mailer->send($message);
+    echo json_decode($result);
+}
